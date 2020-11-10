94ac05917ccbbd6b2cf48b46bd984fd7b2a72689
Alexander Kier Christensen <akc17@ic.ac.uk>
2020-11-10 20:01:57 +0000

    PAssword security

--- SantaBot.py
+++ SantaBot.py
@@ -81,7 +81,7 @@ timestamp = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
 port = 465  # for SSL
 smtp_server = "smtp.gmail.com"
 sender_email = "santabot69@gmail.com"
-pw = "santababy"
+pw = input("Type your password and press enter:")
 context = ssl.create_default_context()
 with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
     server.login(sender_email, pw)
